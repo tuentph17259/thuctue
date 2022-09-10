23826ea19e7f53d8290b51b8b503bc1c55bd95ae
TrinhThuc <trinhthuc432@gmail.com>
2022-09-10 11:23:09 +0700

    adsadf

---
+++ src/main/java/com/example/thuctue/config/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.thuctue.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+}
