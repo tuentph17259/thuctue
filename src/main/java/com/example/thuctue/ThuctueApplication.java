403dfea835e4c78a8452a2c95d180d026bf5146d
TrinhThuc <trinhthuc432@gmail.com>
2022-09-10 11:14:53 +0700

    lkjsdbfkabdsf

---
+++ src/main/java/com/example/thuctue/ThuctueApplication.java
@@ -0,0 +1,13 @@
+package com.example.thuctue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuctueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThuctueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thuctue/ThuctueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuctue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuctueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
